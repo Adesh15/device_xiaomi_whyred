1f8d0fd04b1e07829ae2d0bcfbfa80987733c6ae
Kevin Rocard <krocard@google.com>
2018-03-02 19:41:31 -0800

    whyred: Migrate to audio V4
    Test: regression test on the board
    Bug: 38184704
    Change-Id: Ife559125850277865451f92ea862e7ec47fc9da7
    Merged-In: Ife559125850277865451f92ea862e7ec47fc9da7
    Cherry-picked from master
    Signed-off-by: Kevin Rocard <krocard@google.com>

--- device.mk
+++ device.mk
@@ -76,10 +76,9 @@ TARGET_SCREEN_WIDTH := 1080
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.broadcastradio@1.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
     audio.a2dp.default \
     audio.primary.sdm660 \
     audio.r_submix.default \
