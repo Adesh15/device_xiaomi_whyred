a12e2076030b7e490750cd754d7999ffde80f651
guaiyihu <guaiyihu@foxmail.com>
2018-09-18 08:41:19 +0800

    whyred: proprietary: Unpin libicuuc and libminikin
     * Patch the file name automatically via extract-files.sh
     * Move libskia to vendor
     * Drop 64-bit libskia
    
    Change-Id: I151c676c04768a2b51612a57c6e0b0f2897d8bc7

--- extract-files.sh
+++ extract-files.sh
@@ -63,6 +63,8 @@ setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" true "$CLEAN_VENDOR"
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 
 BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+patchelf --set-soname libicuuc-v27.so $BLOB_ROOT/vendor/lib/libicuuc-v27.so
+patchelf --set-soname libminikin-v27.so $BLOB_ROOT/vendor/lib/libminikin-v27.so
 
 patchelf --replace-needed android.frameworks.sensorservice@1.0.so android.frameworks.sensorservice@1.0-v27.so $BLOB_ROOT/vendor/lib/libvidhance_gyro.so
 patchelf --replace-needed libminikin.so libminikin-v27.so $BLOB_ROOT/vendor/lib/libMiWatermark.so
