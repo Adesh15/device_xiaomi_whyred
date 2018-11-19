e938c8459b341a8ed6a24cb16c4b88fc33ea3d57
Adesh15 <adesikha15@gmail.com>
2018-11-19 05:33:52 +0000

    whyred: Disable BOARD_HAVE_QCOM_FM
    Signed-off-by: Adesh15 <adesikha15@gmail.com>
    Signed-off-by: thelazyindian <satyabrat.me@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -162,7 +162,7 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/class/input/input2/wake_gesture"
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # FM
-BOARD_HAVE_QCOM_FM := true
+#BOARD_HAVE_QCOM_FM := true
 BOARD_HAS_QCA_FM_SOC := "cherokee"
 
 # GPS
